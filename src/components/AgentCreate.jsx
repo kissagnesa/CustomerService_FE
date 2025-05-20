d1ecb057f9ca2af413efa9c035f32f3adce8dae4
kissagnesa <sindzse88@gmail.com>
2025-05-20 14:26:57 +0200

    Agent crud files created

--- src/components/AgentCreate.jsx
+++ src/components/AgentCreate.jsx
@@ -4,12 +4,12 @@ export const CustomerServiceCreatePage=()=>{
     const navigate = useNavigate();
     return (
         <div className="p-5 content bg-whitesmoke text-center">
-            <h2>Új hívás</h2>
+            <h2>Új kolléga felvétele</h2>
             <form
             onSubmit={(event) => {
             event.persist();
             event.preventDefault();
-            fetch(`https://localhost:7038/swagger/index.html`, {
+            fetch(`http://localhost:5036/Agent/NewAgent`, {
                 method: "POST",
                
                 headers: {
@@ -17,12 +17,7 @@ export const CustomerServiceCreatePage=()=>{
                 },
                 
                 body: JSON.stringify({
-                    customerName: event.target.elements.customerName.value,
-                    agentName: event.target.elements.agentName.value,
-                    phoneNumber: event.target.elements.phoneNumber.value,
-                    callDate: event.target.elements.callDate.value,
-                    callTime: event.target.elements.callTime.value
-                   
+                    agentName: event.target.elements.agentName.value                   
                 }),
             })
             
@@ -33,15 +28,15 @@ export const CustomerServiceCreatePage=()=>{
             .catch(console.log);
             }}>
             <div className="form-group row pb-3">
-                <label className="col-sm-3 col-form-label">ITMP bejegyzés</label>
+                <label className="col-sm-3 col-form-label">Új kolléga felvétele</label>
                 <div className="col-sm-9">
                 <input type="text" name="name" className="form-control" />
                 </div>
             </div>
             <div className="form-group row pb-3">
-                <label className="col-sm-3 col-form-label">Email cím: </label>
+                <label className="col-sm-3 col-form-label">Név </label>
                 <div className="col-sm-9">
-                <input type="email" name="email" className="form-control" />
+                <input type="name" name="name" className="form-control" />
                 </div>
             </div>           
             
